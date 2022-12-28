f9da92607033cf067488152a99866a08ff4d03ca
Satya Ranjan <ranjansatyaa98@gmail.com>
2022-12-29 00:28:33 +0530

    challenge3

---
+++ challenge3/nestedobject.py
@@ -0,0 +1,24 @@
+def recursive_items(dictionary,key_str):
+    key_lst = key_str.split('/')
+       temp_dict = dictionary
+    for i in key_lst:
+        if type(temp_dict[i]) is dict:
+            temp_dict = temp_dict.get(i)
+        else:
+           return temp_dict[i]
+object1 = {"a":{"b":{"c":"d"}}}
+object2 = {"x":{"y":{"z":"a"}}}
+
+
+print(recursive_items(object2,"x/y/z"))
+
+
+
+########Test case#############
+
+# Output - 
+
+# satya@linux:~$ python3 nestedobject.py
+# a
+
+###############################################
